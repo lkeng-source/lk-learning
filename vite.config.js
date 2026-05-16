f59f58a2244fdb0376752eb0764b7663105151f3
lkeng-source <lkeng.general@gmail.com>
2026-05-16 12:40:09 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// GitHub Pages 路徑為 /lk-learning/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/lk-learning/',
   server: {
     port: 5173,
     host: true
